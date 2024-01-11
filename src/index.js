4b1c0806608a6d9f4b300a3ccd43971ffd8cb932
nesty11 <arielvale8@hotmail.com>
2024-01-11 17:21:32 -0500

    formatted code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
+import * as atatus from "atatus-spa";
+atatus.config("eda29bd004ad48eb984553eba7edf300").install();
 
-import * as atatus from 'atatus-spa';
-atatus.config('eda29bd004ad48eb984553eba7edf300').install();
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -24,5 +23,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-atatus.notify(new Error('Test Atatus Setup'));
+atatus.notify(new Error("Test Atatus Setup"));
